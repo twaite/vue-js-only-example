9aa9c6a031a8206a284149610a291fa2093ef8a5
Tim Waite <tim@dialexa.com>
2019-02-04 22:55:22 -0600

    Initial commit, styled components working

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import StyledButton from "./components/test";
+
+
+const App = {
+  name: 'app',
+  render() {
+    return (
+      <div id="app">
+        <h1>Hello World</h1>
+        <StyledButton>Test</StyledButton>
+      </div>
+    );
+  },
+}
+
+export default App;
